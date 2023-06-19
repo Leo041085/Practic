94d5c14839bf71c6b5d149ae13e7607671c73555
Leo <fullsleons@gmail.com>
2023-06-19 17:31:38 +0300

    scroll

--- src/index.js
+++ src/index.js
@@ -1,65 +1,89 @@
-// api key: 345007f9ab440e5b86cef51be6397df1
-// documentation: https://developer.themoviedb.org/docs/getting-started, https://developer.themoviedb.org/reference/trending-movies
-
-// img : https://developer.themoviedb.org/docs/image-basics
-// scroll : https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
-
 // const moviesList = document.querySelector('.js-list');
 // const btnLoad = document.querySelector('.js-btn');
-// const guard = document.querySelector('.js-guard');
-// let page = 1;
+// let page = 498;
+
+// function serviceMovie(page = 1) {
+//     const BASE_URL = 'https://api.themoviedb.org/3';
+//     const END_POINT = '/trending/movie/week';
+//     const API_KEY = '345007f9ab440e5b86cef51be6397df1';
+
+//     return fetch(`${BASE_URL}${END_POINT}?api_key=${API_KEY}&page=${page}`).then((response) => {
+//         if (!response.ok) {
+//         throw new Error(response.status)
+//         }
+//         return response.json();
+// })
+// };
+
+// serviceMovie().then((data) => {
+//     moviesList.insertAdjacentHTML('beforeend', createMarkup(data.results));
+//     btnLoad.hidden = false;
+// });
+
+// btnLoad.addEventListener('click', handlerBtnLoadMore);
+
+// function handlerBtnLoadMore() {
+//     page += 1;
+//     serviceMovie(page).then((data) => {
+//         moviesList.insertAdjacentHTML('beforeend', createMarkup(data.results));
+//         if (data.page >= 500) {
+//             btnLoad.hidden = true;
+//         }
+//     });
+// };
+
+// function createMarkup(arr) {
+//     return arr.map(({ id, title, poster_path }) => {
+//         return `<li class="js-item" data-id='${id}'>
+//           <img src="https://image.tmdb.org/t/p/w400${poster_path}" alt="${title}">
+//           <h3>${title}</h3>
+//         </li>`
+//     }).join('');
+// };
+
+// ===============================PAGINATION WITH INFINITY SCROLL====================
+
+const moviesList = document.querySelector('.js-list');
+const quard = document.querySelector('.js-quard');
+let page = 1;
 
 function serviceMovie(page = 1) {
   const BASE_URL = 'https://api.themoviedb.org/3';
   const END_POINT = '/trending/movie/week';
   const API_KEY = '345007f9ab440e5b86cef51be6397df1';
+
   return fetch(`${BASE_URL}${END_POINT}?api_key=${API_KEY}&page=${page}`).then(
-    resp => {
-      if (!resp.ok) {
-        throw new Error(resp.status);
+    response => {
+      if (!response.ok) {
+        throw new Error(response.status);
       }
-      return resp.json();
+      return response.json();
     }
   );
 }
-// serviceMovie().then(data => {
-//   moviesList.insertAdjacentHTML('beforeend', createMarkup(data.results));
-//   btnLoad.hidden = false;
-// });
-// btnLoad.addEventListener('click', handlerBtnLoadMore);
 
-// function handlerBtnLoadMore() {
-//   page += 1;
-//     serviceMovie(page).then(data => {
-//       moviesList.insertAdjacentHTML('beforeend', createMarkup(data.results));
-//     if (data.page >= 500) {
-//       btnLoad.hidden = true;
-//     }
-//   });
-// }
+serviceMovie().then(data => {
+  moviesList.insertAdjacentHTML('beforeend', createMarkup(data.results));
+  observer.observe(quard);
+});
 
-// function createMarkup(array) {
-//   return array
-//     .map(({ id, title, poster_path }) => {
-//       return `<li class="js-item" data-id='${id}'>
-//             <img src="https://image.tmdb.org/t/p/w400${poster_path}" alt="${title}">
-//             <h3>${title}</h3>
-//         </li>`;
-//     })
-//     .join('');
-// }
-
-// scroll
-const moviesList = document.querySelector('.js-list');
-const guard = document.querySelector('.js-guard');
-let page = 1;
+function createMarkup(arr) {
+  return arr
+    .map(({ id, title, poster_path }) => {
+      return `<li class="js-item" data-id='${id}'>
+          <img src="https://image.tmdb.org/t/p/w400${poster_path}" alt="${title}">
+          <h3>${title}</h3>
+        </li>`;
+    })
+    .join('');
+}
 
 const options = {
   root: null,
   rootMargin: '500px',
   threshold: 0,
 };
-const observer = new IntersectionObserver(loadMore, options);
+
 function loadMore(entries, observer) {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
@@ -67,24 +91,11 @@ function loadMore(entries, observer) {
       serviceMovie(page).then(data => {
         moviesList.insertAdjacentHTML('beforeend', createMarkup(data.results));
         if (data.page >= 500) {
-          observer.unobserve(guard);
+          observer.unobserve(quard);
         }
       });
     }
   });
 }
-function createMarkup(array) {
-  return array
-    .map(({ id, title, poster_path }) => {
-      return `<li class="js-item" data-id='${id}'>
-            <img src="https://image.tmdb.org/t/p/w400${poster_path}" alt="${title}">
-            <h3>${title}</h3>
-        </li>`;
-    })
-    .join('');
-}
-serviceMovie().then(data => {
-    moviesList.insertAdjacentHTML('beforeend', createMarkup(data.results));
-    console.log(guard);
-    observer.observe(guard)
-});
+
+const observer = new IntersectionObserver(loadMore, options);
